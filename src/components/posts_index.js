57b991c3d3f43588d19dd6ce982cd7615712e0d3
kevinhsu <kevinhsu@synology.com>
2017-03-31 00:07:25 +0800

    es6 refine

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
@@ -15,4 +14,4 @@ class PostsIndex extends Component {
   }
 }
 
-export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+export default connect(null, { fetchPosts })(PostsIndex);
